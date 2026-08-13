037e89f9206755a187bae0d209d7a9d94727aee9
birukKE <158283227+birukKE@users.noreply.github.com>
2026-08-13 13:29:15 +0200

    Delete src/exam.py

--- src/exam.py
+++
@@ -1,8 +0,0 @@
-
-
-names = ["Alexa","Siri","Cortana", "Debra", "Dexter", "Batista"]
-# print(" ".join("Hello there ",(i for i in names)))
-
-str = " ".join( "I know " + name if i%2 == 0 else "I don't know " + name for i , name in enumerate(names))
-
-print(str)
