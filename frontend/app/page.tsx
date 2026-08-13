3542bbba9309d6d340893759ab4ae1629e52b1c2
birukKE <158283227+birukKE@users.noreply.github.com>
2026-08-13 13:36:39 +0200

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -22,18 +22,13 @@ export default function Home() {
   const [current_session_counter, set_current_session_counter] = useState(0)
   const [hasDeletedChat, setHasDeletedChat] = useState(false)
   
-  // const [agentAnswers, setAgentAnswers] = useState<String[]>([])
 
   useEffect(() =>{
-    console.log("Fetching")
     fetch('http://localhost:5000/get_initial_renders')
     .then((response) => response.json())
     .then((data) => {
-      console.log("The data is: ", data)
       setChatRoomButtons([...data['session_counter']])
       setButtonTitle([...data['session_id']])
-      console.log("XXXX data['session_counter'] = ", data['session_counter'])
-      console.log("done now: ",(data))
     })
     .catch(error => console.log(error))
   }, [isHistoryButtonPressed, hasDeletedChat])
@@ -42,7 +37,6 @@ export default function Home() {
     fetch('http://localhost:5000/get_initial_renders')
     .then((response) => response.json())
     .then((data) => {
-      console.log("The data is: ", data)
       setChatRoomButtons([...data['session_counter']])
       setButtonTitle([...data['session_id']])
     })
@@ -51,10 +45,7 @@ export default function Home() {
 
 
   const SendMessageToBackend = () => {
-    console.log("Entered SendMessageToBackend: "  + currentInputValue)
-    // console.log("yeah yeah current_session_id = ", current_session_id[0], "   |   current_session_counter", current_session_counter)
     let is_new_chat_this_call = true
-    // check if this is the first message in this chat so I can create a new session_id for it    
     if (currentInputValue !== ""){      
       let is_new_chat = textMessage.length == 0? true: false
       let message_to_send = {"is_new_chat": is_new_chat,
@@ -62,9 +53,7 @@ export default function Home() {
                              'current_session_id': current_session_id,
                              'current_session_counter': current_session_counter
                             }
-      console.log("yeah yeah current_session_id = ", current_session_id, "   |   current_session_counter", current_session_counter)
 
-      console.log("message_to_send: ", message_to_send);
       fetch('http://localhost:5000/user_query', {
         method:'POST',
         headers: {"Content-Type": "application/json"}, 
@@ -72,16 +61,12 @@ export default function Home() {
       })
       .then(response => response.json())
       .then(data => {
-        console.log(data)
         setTextMessage(prev => [... prev, {'messenger': 'ai', 'message': data['ai_response']}])
         setIsHistoryButtonPressed(!isHistoryButtonPressed)
         if (is_new_chat == true){
-          console.log("is in is new chat, are we in the if")
           set_current_session_counter(data['current_session_counter'])
           set_current_session_id(data['current_session_id'])
-          console.log("no no current_session_id = ", data['current_session_id'], "   |   current_session_counter", data['current_session_counter'])
         }
-        console.log("no no current_session_id = ", current_session_id, "   |   current_session_counter", current_session_counter)
       })
       .catch(error => console.log(error))
     }   
